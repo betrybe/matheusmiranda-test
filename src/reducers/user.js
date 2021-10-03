ef9f3477eb6a43556b47022125f5e270c465e6bc
mtmiranda <mattheus_miranda@hotmail.com>
2021-10-03 14:17:38 -0300

    creating global reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,24 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+
+};
+
+const UserReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'SET_EMAIL':
+    return { ...state, email: action.payload.email };
+
+  default:
+    console.log('Empty email');
+  }
+
+  return state;
+};
+
+export default UserReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const initialState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+
+};
+
+const WalletReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'SET_EXPENSES':
+    return { ...state, expenses: action.payload.expenses };
+
+  default:
+    console.log('Empty email');
+  }
+
+  return state;
+};
+
+export default WalletReducer;
