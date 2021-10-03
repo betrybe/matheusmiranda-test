14914b4005612afdbb92c3f77efe43ef9e2b6fcd
mtmiranda <mattheus_miranda@hotmail.com>
2021-10-03 14:08:23 -0300

    creating store and provider from redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/global.css';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={ store }>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
 
